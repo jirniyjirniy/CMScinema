e4232edbc09886328320a81b467aacd8127c5442
jirniy <nikitach.fw@gmail.com>
2024-04-21 15:46:07 +0300

    Все страницы адаптированы

--- core/settings.py
+++ core/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'asdasdaas'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '64.226.115.177']
 
 
 # Application definition
